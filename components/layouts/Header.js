c74e37662471227b49fde28f233a17429d85a436
Roshan Bhatia <prakashbhatia1970@gmail.com>
2024-04-20 17:39:04 +0530

    Header Section 2 Completed

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -23,48 +23,59 @@ const Header = () => {
 
         <React.Fragment>
 
-       {/* Header Section */}
+            {/* Header Section */}
 
-        <View style={HeaderStyles.body}>
-            <View style={HeaderStyles.headerParent}>
-                <View style={HeaderStyles.headerItems}>
-                    <Image source={arrowButtonImage} style={{height: 30,}}>
+            <View style={HeaderStyles.body}>
+                <View style={HeaderStyles.headerParent}>
+                    <View style={HeaderStyles.headerItems}>
+                        <Image source={arrowButtonImage} style={{ height: 30, marginLeft: 14 }}>
 
-                    </Image>
+                        </Image>
+                    </View>
+
+                    <View style={HeaderStyles.headerItems}>
+                        <Text style={HeaderStyles.heading}> Physician Orders </Text>
+                    </View>
+
+                    <View style={HeaderStyles.headerItems}>
+                        <Image source={notificationBellImage} style={HeaderStyles.headerImages}>
+
+                        </Image>
+                    </View>
+
                 </View>
 
-                <View style={HeaderStyles.headerItems}>
-                    <Text style={HeaderStyles.heading}> Physician Orders </Text>
+            </View>
+
+            {/* Navigation Menu Section */}
+
+            <View style={HeaderStyles.navigationMenu}>
+
+                {/* <View style={[HeaderStyles.navigationItems, HeaderStyles.navigationActiveItem]}>
+                    <Text style={HeaderStyles.navigationItemTextActive}> New <Text style={[HeaderStyles.activeNotifications,HeaderStyles.navigationItemTextActive]}>
+                        3
+                    </Text> </Text>
+                </View> */}
+
+                <View style={[HeaderStyles.navigationItems, HeaderStyles.navigationActiveItem]}>
+                    <Text style={HeaderStyles.navigationItemTextActive}> New </Text><View style={HeaderStyles.activeNotifications}><Text style={HeaderStyles.innertextActive}>
+                        3
+                    </Text></View>
                 </View>
 
-                <View style={HeaderStyles.headerItems}>
-                <Image source={notificationBellImage} style={HeaderStyles.headerImages}>
-
-</Image>
+                <View style={HeaderStyles.navigationItems}>
+                    <Text style={HeaderStyles.navigationItemText}> Fulfilled  <Text style={HeaderStyles.Notifications}> 6 </Text>  </Text>
                 </View>
 
-            </View>
+                <View style={HeaderStyles.navigationItems}>
+                    <Text style={HeaderStyles.navigationItemText}> Cancelled  <Text style={HeaderStyles.Notifications}> 2 </Text> </Text>
+                </View>
 
-        </View>
-
-        {/* Navigation Menu Section */}
-
-        <View style={HeaderStyles.navigationMenu}>
-
-            <View style={HeaderStyles.navigationItems}>
 
             </View>
 
-            <View style={HeaderStyles.navigationItems}>
-                
-            </View>
+            <View style={HeaderStyles.horizontalLine}></View>
 
-            <View style={HeaderStyles.navigationItems}>
-                
-            </View>
-
-
-        </View>
         </React.Fragment>
     )
 
@@ -76,31 +87,107 @@ const HeaderStyles = StyleSheet.create({
         marginTop: '15%',
     },
 
-    headerParent:{
-       
+    headerParent: {
+
         padding: 20,
         flex: 0,
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         verticalAlign: 'middle',
+        width: '100%',
+        textAlign: 'center',
     },
 
-    headerImages:{
-        height: 50,
-        
+    headerImages: {
+        objectFit: 'contain',
+        width: 50,
+
+
     },
 
-    heading:{
+    heading: {
         fontSize: 20,
         fontWeight: '700',
         color: Colors.blackPrimary,
+        textAlign: 'center',
 
     },
 
-    headerItems:{
+    headerItems: {
         verticalAlign: 'middle',
-    }
+    },
+
+    navigationMenu: {
+        flex: 0,
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        width: '100%',
+    },
+
+    navigationItems: {
+        borderRadius: 20,
+        marginVertical: 4,
+        flex: 0,
+        flexDirection: 'row',
+        alignItems: 'center',
+        
+    },
+
+    navigationItemText: {
+        padding: 10,
+        fontWeight: '700',
+        color: Colors.greyPrimary,
+
+    },
+
+    navigationActiveItem: {
+        backgroundColor: Colors.blueSecondary,
+        
+
+
+    },
+
+    navigationItemTextActive: {
+        color: Colors.bluePrimary,
+        padding: 10,
+        fontWeight: '700',
+
+    },
+
+    innertextActive: {
+        fontWeight: '700',
+        color: Colors.bluePrimary,
+        paddingVertical: 1,
+        paddingHorizontal: 9,
+        borderRadius: 50,
+    },
+
+    // Notifications
+
+    activeNotifications: {
+        backgroundColor: 'rgba(28, 170, 212, 0.09)',
+        borderRadius: 30,
+        padding: 2,
+        margin:10,
+        flex: 0,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    Notifications: {
+        backgroundColor: Colors.greySecondary,
+    },
+
+    // Horizontal Line
+
+    horizontalLine: {
+        borderBottomColor: '#EFEFEF',
+        borderBottomWidth: 2,
+
+    },
 
 
 
--- styles/colors.js
+++ styles/colors.js
@@ -2,6 +2,8 @@ const Colors = {
     blackPrimary: '#18181B',
     subheading: '#71717A',
     greyPrimary: '#A0A0A0',
+    greySecondary:'#F9FAFB',
+    lightGreyPrimary: '#EFEFEF',
     bluePrimary: '#1CAAD4',
     blueSecondary: '#EDF8FB',
     cyanPrimary: '#53D2BB',
